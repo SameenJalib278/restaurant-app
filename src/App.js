84124a1e5f94dbbb2478c5e873ea60b5715b100c
John Doe <johndoe@example.com>
2021-12-25 14:26:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Restaurant from './component/basic/Restaurant';
+import UseState from './component/Hooks/UseState';
+import UseEffect from './component/Hooks/UseEffect';
+import Todo from './component/todoreact/Todo';
+import Temp from "./component/weather/Temp";
+
+const App = () => {
+  return   <Restaurant />;
+ 
+  // <UseState />; 
+  // return <UseEffect />;
+  // <Todo />;
+ // <Temp />; 
+};
+
+export default App;
